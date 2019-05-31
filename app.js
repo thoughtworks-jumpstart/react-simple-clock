73192999fd4ea2dcf8dadae564f36f747a21dcd0
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-05-31 12:11:28 +0800

    Add initial implementation of clock app

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const element = (
-  <div>
-    <h1>What's the time now?</h1>
-    <h2>It is {new Date().toLocaleTimeString()}</h2>
-  </div>
-);
+function Clock(props) {
+  return (
+    <div>
+      <h1>What's the time now?</h1>
+      <h2>It is {props.date}</h2>
+    </div>
+  );
+}
 
-const container = document.getElementById("app");
+function tick() {
+  const date = new Date().toLocaleTimeString();
+  console.log("happy");
+  const element = <Clock date={date} />;
+  const container = document.getElementById("app");
+  ReactDOM.render(element, container);
+}
 
-ReactDOM.render(element, container);
-
-// Wrap the above code in a function block and use setInterval to call it every second
+setInterval(tick, 1000);
