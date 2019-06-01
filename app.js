eb07df7c3682a49830951d1e69f4353afb42b0dc
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-06-01 13:29:43 +0800

    Rename date to time

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ function Clock(props) {
   return (
     <div>
       <h1>What's the time now?</h1>
-      <h2>It is {props.date}</h2>
+      <h2>It is {props.time}</h2>
     </div>
   );
 }
 
 function tick() {
-  const date = new Date().toLocaleTimeString();
-  console.log("happy");
-  const element = <Clock date={date} />;
+  const time = new Date().toLocaleTimeString();
+  const element = <Clock time={time} />;
   const container = document.getElementById("app");
   ReactDOM.render(element, container);
 }
