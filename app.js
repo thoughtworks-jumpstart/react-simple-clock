c62aac61eff82e2254dee5532fc5c04ad2124204
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-06-01 15:01:52 +0800

    Add solution

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-function Clock(props) {
-  return (
-    <div>
-      <h1>What's the time now?</h1>
-      <h2>It is {props.time}</h2>
-    </div>
-  );
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    const time = new Date().toLocaleTimeString();
+    this.state = { time };
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      const time = new Date().toLocaleTimeString();
+      this.setState({ time });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>What's the time now?</h1>
+        <h2>It is {this.state.time}</h2>
+      </div>
+    );
+  }
 }
 
 function tick() {
-  const time = new Date().toLocaleTimeString();
-  const element = <Clock time={time} />;
+  const element = <Clock />;
   const container = document.getElementById("app");
   ReactDOM.render(element, container);
 }
